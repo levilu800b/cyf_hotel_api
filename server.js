9665649a8b25c92ecccfcef00a03dea6be5136f6
levilu800b <levilu800b@gmail.com>
2023-02-18 12:57:54 +0000

    pg

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.listen(3000, function () {
-	console.log('Server is listening on port 3000. Ready to accept requests!');
+app.use(bodyParser.json());
+
+const db = new Pool ({
+    user: 'LeviL',
+    host: 'localhost',
+    database: 'cyf_hotel',
+    password: '',
+    port: 5432
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hotel Database Project Home Page</h1>')
 });
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
